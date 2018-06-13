1616eb5ce27ce22860672e1842eb8b5ac51bcd80
SMG <scandanavianmountaingoat@gmail.com>
2018-06-13 14:22:59 -0500

    test rgb to hsv floats

--- test/SFGraphicsTest/ColorToolsTest.cs
+++ test/SFGraphicsTest/ColorToolsTest.cs
@@ -167,6 +167,49 @@ namespace SFGraphicsTest
         }
 
         [TestClass]
+        public class RgbToHsvFloatTest
+        {
+            [TestMethod]
+            public void RgbToHsvBlack()
+            {
+                float h;
+                float s;
+                float v;
+                ColorTools.RgbToHsv(0, 0, 0, out h, out s, out v);
+
+                Assert.AreEqual(0, h);
+                Assert.AreEqual(0, s);
+                Assert.AreEqual(0, v);
+            }
+
+            [TestMethod]
+            public void RgbToHsvWhite()
+            {
+                float h;
+                float s;
+                float v;
+                ColorTools.RgbToHsv(1, 1, 1, out h, out s, out v);
+
+                Assert.AreEqual(0, h);
+                Assert.AreEqual(0, s);
+                Assert.AreEqual(1, v);
+            }
+
+            [TestMethod]
+            public void RgbToHsvRed()
+            {
+                float h;
+                float s;
+                float v;
+                ColorTools.RgbToHsv(1, 0, 0, out h, out s, out v);
+
+                Assert.AreEqual(0, h);
+                Assert.AreEqual(1, s);
+                Assert.AreEqual(1, v);
+            }
+        }
+
+        [TestClass]
         public class FloatToIntClampTest
         {
             [TestMethod]
