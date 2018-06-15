95c955838d25d13d12f59ad7835d153385b8b586
SMG <scandanavianmountaingoat@gmail.com>
2018-06-15 13:34:40 -0500

    constructor for compressed textures with mipmaps

--- src/SFGraphics/GLObjects/Textures/Texture2D.cs
+++ src/SFGraphics/GLObjects/Textures/Texture2D.cs
@@ -34,5 +34,47 @@ namespace SFGraphics.GLObjects.Textures
 
             GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
         }
+
+        /// <summary>
+        /// Initializes a texture of the specified format and loads all specified mipmaps.
+        /// </summary>
+        /// <param name="width">The width of the base mip level</param>
+        /// <param name="height">The height of the base mip level</param>
+        /// <param name="mipmaps">A list of byte arrays for each mip level</param>
+        /// <param name="pixelInternalFormat"></param>
+        public Texture2D(int width, int height, List<byte[]> mipmaps, PixelInternalFormat pixelInternalFormat) : base(TextureTarget.Texture2D, width, height)
+        {
+            Bind();
+            
+            // Load the first level.
+            GL.CompressedTexImage2D<byte>(TextureTarget.Texture2D, 0, pixelInternalFormat, width, height, 0, mipmaps[0].Length, mipmaps[0]);
+
+            // The number of mip maps needs to be specified first.
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMaxLevel, mipmaps.Count);
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+
+            // Initialize the data for each level.
+            for (int mipLevel = 0; mipLevel < mipmaps.Count; mipLevel++)
+            {
+                GL.CompressedTexImage2D<byte>(TextureTarget.Texture2D, mipLevel, pixelInternalFormat,
+                    width / (int)Math.Pow(2, mipLevel), height / (int)Math.Pow(2, mipLevel), 0, mipmaps[mipLevel].Length, mipmaps[mipLevel]);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a texture of the specified format. The additional mip levels are generated.
+        /// </summary>
+        /// <param name="width">The width of the base mip level</param>
+        /// <param name="height">The height of the base mip level</param>
+        /// <param name="baseMipLevel">The data for the base mip level.</param>
+        /// <param name="pixelInternalFormat"></param>
+        public Texture2D(int width, int height, byte[] baseMipLevel, PixelInternalFormat pixelInternalFormat) : base(TextureTarget.Texture2D, width, height)
+        {
+            Bind();
+
+            // Load the first level and generate the rest.
+            GL.CompressedTexImage2D<byte>(TextureTarget.Texture2D, 0, pixelInternalFormat, width, height, 0, baseMipLevel.Length, baseMipLevel);
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+        }
     }
 }
