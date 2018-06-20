2c65edd46b66ac43a808ba68bd46811e4006b348
SMG <scandanavianmountaingoat@gmail.com>
2018-06-20 14:58:05 -0500

    test for black tan/bitan

--- test/SFGraphicsTest/VectorToolsTest.cs
+++ test/SFGraphicsTest/VectorToolsTest.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Drawing;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenTK;
 using SFGraphics.Tools;
+using System;
 
 namespace SFGraphicsTest
 {
@@ -92,28 +91,46 @@ namespace SFGraphicsTest
         [TestClass]
         public class GenerateTangentBitangentTest
         {
-            private readonly double delta = 0.0001;
-
             [TestMethod]
-            public void DifferentUVs()
+            public void DifferentUVsDifferentPositions()
             {
-                // TODO: Not implemented.
-                Assert.Fail();
+                Vector3 v1 = new Vector3(1, 0, 0);
+                Vector3 v2 = new Vector3(0, 1, 0);
+                Vector3 v3 = new Vector3(0, 0, 1);
+                Vector2 uv1 = new Vector2(1, 0);
+                Vector2 uv2 = new Vector2(0, 1);
+                Vector2 uv3 = new Vector2(1, 1);
+                Vector3 s;
+                Vector3 t;
+                VectorTools.GenerateTangentBitangent(v1, v2, v3, uv1, uv2, uv3, out s, out t);
+
+                // Make sure tangents and bitangents aren't all zero.
+                Assert.IsTrue((Math.Abs(s.X) > 0) || (Math.Abs(s.Y) > 0) || (Math.Abs(s.Z) > 0));
+                Assert.IsTrue((Math.Abs(t.X) > 0) || (Math.Abs(t.Y) > 0) || (Math.Abs(t.Z) > 0));
             }
 
             [TestMethod]
-            public void SameUVs()
+            public void SameUVsDifferentPositions()
             {
-                // TODO: Not implemented.
-                Assert.Fail();
+                Vector3 v1 = new Vector3(1, 0, 0);
+                Vector3 v2 = new Vector3(0, 1, 0);
+                Vector3 v3 = new Vector3(0, 0, 1);
+                Vector2 uv1 = new Vector2(1, 1);
+                Vector2 uv2 = new Vector2(1, 1);
+                Vector2 uv3 = new Vector2(1, 1);
+                Vector3 s;
+                Vector3 t;
+                VectorTools.GenerateTangentBitangent(v1, v2, v3, uv1, uv2, uv3, out s, out t);
+
+                // Make sure tangents and bitangents aren't all zero.
+                Assert.IsTrue((Math.Abs(s.X) > 0) || (Math.Abs(s.Y) > 0) || (Math.Abs(s.Z) > 0));
+                Assert.IsTrue((Math.Abs(t.X) > 0) || (Math.Abs(t.Y) > 0) || (Math.Abs(t.Z) > 0));
             }
         }
 
         [TestClass]
         public class GenerateNormalsTest
         {
-            private readonly double delta = 0.0001;
-
             [TestMethod]
             public void PositiveNormal()
             {
