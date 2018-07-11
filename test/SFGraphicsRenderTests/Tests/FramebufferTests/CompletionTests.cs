d76c0b3656d93921784b088a38ae8eaeef848cf0
SMG <scandanavianmountaingoat@gmail.com>
2018-07-11 12:16:45 -0500

    framebuffer completion tests

---
+++ test/SFGraphicsRenderTests/Tests/FramebufferTests/CompletionTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SFGraphics.GLObjects;
+using OpenTK.Graphics.OpenGL;
+
+namespace SFGraphicsRenderTests.Tests.FramebufferTests
+{
+    [TestClass]
+    public class CompletionTests
+    {
+        [TestInitialize()]
+        public void Initialize()
+        {
+            // Set up the context for all the tests.
+            TestTools.OpenTKWindowlessContext.CreateDummyContext();
+        }
+
+        [TestMethod]
+        public void NoAttachments()
+        {
+            // This is missing a depth attachment.
+            Framebuffer framebuffer = new Framebuffer(FramebufferTarget.Framebuffer);
+            Assert.AreEqual("FramebufferIncompleteMissingAttachment", framebuffer.GetStatus());
+        }
+
+        [TestMethod]
+        public void JustDepthAttachments()
+        {
+            Framebuffer framebuffer = new Framebuffer(FramebufferTarget.Framebuffer, 1, 1, PixelInternalFormat.Rgba, 0);
+            Assert.AreEqual("FramebufferCompleteExt", framebuffer.GetStatus());
+        }
+
+        [TestMethod]
+        public void OneColorAttachment()
+        {
+            Framebuffer framebuffer = new Framebuffer(FramebufferTarget.Framebuffer, 1, 1, PixelInternalFormat.Rgba, 1);
+            Assert.AreEqual("FramebufferCompleteExt", framebuffer.GetStatus());
+        }
+
+        [TestMethod]
+        public void MultipleColorAttachments()
+        {
+            Framebuffer framebuffer = new Framebuffer(FramebufferTarget.Framebuffer, 1, 1, PixelInternalFormat.Rgba, 3);
+            Assert.AreEqual("FramebufferCompleteExt", framebuffer.GetStatus());
+        }
+    }
+}
