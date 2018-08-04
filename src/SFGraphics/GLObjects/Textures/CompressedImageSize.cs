bfed57abef60451ee66c265a4b109c53b6aec619
SMG <scandanavianmountaingoat@gmail.com>
2018-08-04 11:22:24 -0500

    fix imageSize for dxt1 variants

--- src/SFGraphics/GLObjects/Textures/CompressedImageSize.cs
+++ src/SFGraphics/GLObjects/Textures/CompressedImageSize.cs
@@ -9,7 +9,8 @@ namespace SFGraphics.GLObjects.Textures
     public static class CompressedImageSize
     {
         /// <summary>
-        /// Calculates the imageSize parameter for GL.CompressedTexImage.
+        /// Calculates the imageSize parameter for GL.CompressedTexImage. 
+        /// Supports the variants of InternalFormat for DXT1, DXT3, DXT5 
         /// The imageSize should be recalculated for each mip level when reading mipmaps from existing image data.
         /// </summary>
         /// <param name="width">The width of the mip level in pixels</param>
@@ -19,7 +20,9 @@ namespace SFGraphics.GLObjects.Textures
         public static int CalculateImageSize(int width, int height, InternalFormat internalFormat)
         {
             int blockSize = 16; // DXT3/DXT5
-            if (internalFormat == InternalFormat.CompressedRgbaS3tcDxt1Ext)
+
+            // Ignore the parts of the enum we don't care about.
+            if (internalFormat.ToString().ToLower().Contains("dxt1"))
                 blockSize = 8;
 
             int imageSize = blockSize * (int)Math.Ceiling(width / 4.0) * (int)Math.Ceiling(height / 4.0);
