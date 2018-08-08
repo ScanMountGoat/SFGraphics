a36c661750e97525170b43d9f4725e590354a344
SMG <scandanavianmountaingoat@gmail.com>
2018-08-08 11:27:05 -0500

    unit tests for remaining compressed formats

--- test/SFGraphicsRenderTests/Tests/TextureTests/CompressedImageSizeTests.cs
+++ test/SFGraphicsRenderTests/Tests/TextureTests/CompressedImageSizeTests.cs
@@ -30,39 +30,9 @@ namespace SFGraphicsRenderTests.Tests.TextureTests
         }
 
         [TestMethod]
-        public void CompressedRgbS3tcDxt1Ext()
+        public void CompressedR11Eac()
         {
-            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgbS3tcDxt1Ext));
-        }
-
-        [TestMethod]
-        public void CompressedRgbaS3tcDxt1Ext()
-        {
-            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgbaS3tcDxt1Ext));
-        }
-
-        [TestMethod]
-        public void CompressedSrgbS3tcDxt1Ext()
-        {
-            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSrgbS3tcDxt1Ext));
-        }
-
-        [TestMethod]
-        public void CompressedSrgbAlphaS3tcDxt1Ext()
-        {
-            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSrgbAlphaS3tcDxt1Ext));
-        }
-
-        [TestMethod]
-        public void CompressedRgbaS3tcDxt3Ext()
-        {
-            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgbaS3tcDxt3Ext));
-        }
-
-        [TestMethod]
-        public void CompressedRgbaS3tcDxt5Ext()
-        {
-            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgbaS3tcDxt5Ext));
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedR11Eac));
         }
 
         [TestMethod]
@@ -78,9 +48,177 @@ namespace SFGraphicsRenderTests.Tests.TextureTests
         }
 
         [TestMethod]
+        public void CompressedRg()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRg));
+        }
+
+        [TestMethod]
+        public void CompressedRg11Eac()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRg11Eac));
+        }
+
+        [TestMethod]
+        public void CompressedRgb()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgb));
+        }
+
+        [TestMethod]
+        public void CompressedRgb8Etc2()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgb8Etc2));
+        }
+
+        [TestMethod]
+        public void CompressedRgb8PunchthroughAlpha1Etc2()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgb8PunchthroughAlpha1Etc2));
+        }
+
+        [TestMethod]
+        public void CompressedRgba()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgba));
+        }
+
+        [TestMethod]
+        public void CompressedRgba8Etc2Eac()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgba8Etc2Eac));
+        }
+
+        [TestMethod]
+        public void CompressedRgbaBptcUnorm()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgbaBptcUnorm));
+        }
+
+        [TestMethod]
+        public void CompressedRgbaS3tcDxt1Ext()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgbaS3tcDxt1Ext));
+        }
+
+        [TestMethod]
+        public void CompressedRgbaS3tcDxt3Ext()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgbaS3tcDxt3Ext));
+        }
+
+        [TestMethod]
+        public void CompressedRgbaS3tcDxt5Ext()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgbaS3tcDxt5Ext));
+        }
+
+        [TestMethod]
+        public void CompressedRgbBptcSignedFloat()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgbBptcSignedFloat));
+        }
+
+        [TestMethod]
+        public void CompressedRgbBptcUnsignedFloat()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgbBptcUnsignedFloat));
+        }
+
+        [TestMethod]
+        public void CompressedRgbS3tcDxt1Ext()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgbS3tcDxt1Ext));
+        }
+
+        [TestMethod]
+        public void CompressedRgRgtc2()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedRgRgtc2));
+        }
+
+        [TestMethod]
+        public void CompressedSignedR11Eac()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSignedR11Eac));
+        }
+
+        [TestMethod]
         public void CompressedSignedRedRgtc1()
         {
             Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSignedRedRgtc1));
+        }
+
+        [TestMethod]
+        public void CompressedSignedRg11Eac()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSignedRg11Eac));
+        }
+
+        [TestMethod]
+        public void CompressedSignedRgRgtc2()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSignedRgRgtc2));
+        }
+
+        [TestMethod]
+        public void CompressedSrgb()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSrgb));
+        }
+
+        [TestMethod]
+        public void CompressedSrgb8Alpha8Etc2Eac()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSrgb8Alpha8Etc2Eac));
+        }
+
+        [TestMethod]
+        public void CompressedSrgb8Etc2()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSrgb8Etc2));
+        }
+
+        [TestMethod]
+        public void CompressedSrgb8PunchthroughAlpha1Etc2()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSrgb8PunchthroughAlpha1Etc2));
+        }
+
+        [TestMethod]
+        public void CompressedSrgbAlpha()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSrgbAlpha));
+        }
+
+        [TestMethod]
+        public void CompressedSrgbAlphaBptcUnorm()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSrgbAlphaBptcUnorm));
+        }
+
+        [TestMethod]
+        public void CompressedSrgbAlphaS3tcDxt1Ext()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSrgbAlphaS3tcDxt1Ext));
+        }
+
+        [TestMethod]
+        public void CompressedSrgbAlphaS3tcDxt3Ext()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSrgbAlphaS3tcDxt3Ext));
+        }
+
+        [TestMethod]
+        public void CompressedSrgbAlphaS3tcDxt5Ext()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSrgbAlphaS3tcDxt5Ext));
+        }
+
+        [TestMethod]
+        public void CompressedSrgbS3tcDxt1Ext()
+        {
+            Assert.IsTrue(CompressedTexImage2DSucceeded(width, height, InternalFormat.CompressedSrgbS3tcDxt1Ext));
         }
     }
 }
