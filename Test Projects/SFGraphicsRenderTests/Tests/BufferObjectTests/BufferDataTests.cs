86bdfcda6b6ba854c1bd4c9b51a2273cb3017532
SMG <scandanavianmountaingoat@gmail.com>
2018-08-13 14:56:17 -0500

    test reading/writing data to buffer

---
+++ Test Projects/SFGraphicsRenderTests/Tests/BufferObjectTests/BufferDataTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SFGraphics.GLObjects;
+using OpenTK.Graphics.OpenGL;
+
+namespace SFGraphicsRenderTests.BufferObjectTests
+{
+    [TestClass]
+    public class BufferDataTests
+    {
+        [TestInitialize()]
+        public void Initialize()
+        {
+            // Set up the context for all the tests.
+            TestTools.OpenTKWindowlessContext.BindDummyContext();
+        }
+
+        [TestMethod]
+        public void GetFloatBufferData()
+        {
+            float[] data = new float[] { 1.5f, 2.5f, 3.5f };
+            BufferObject buffer = new BufferObject(BufferTarget.ArrayBuffer);
+            buffer.BufferData(data, sizeof(float), BufferUsageHint.StaticDraw);
+
+            float[] readData = buffer.GetBufferData<float>();
+            CollectionAssert.AreEqual(data, readData);
+        }
+    }
+}
