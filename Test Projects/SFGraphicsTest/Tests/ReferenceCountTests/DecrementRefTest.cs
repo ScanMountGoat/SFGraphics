acf5c8a1138cb6efb94867abd6894bd9f91b9f81
SMG <scandanavianmountaingoat@gmail.com>
2018-08-14 12:20:38 -0500

    more reference counting tests

---
+++ Test Projects/SFGraphicsTest/Tests/ReferenceCountTests/DecrementRefTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Concurrent;
+using SFGraphics.GLObjects.GLObjectManagement;
+
+namespace SFGraphicsTest.Tests.ReferenceCountTests
+{
+    [TestClass]
+    public class DecrementRefTest
+    {
+        [TestMethod]
+        public void TryDecrementInvalidReference()
+        {
+            // Doesn't throw exception.
+            ConcurrentDictionary<string, int> refCountByName = new ConcurrentDictionary<string, int>();
+            ReferenceCounting.DecrementReference(refCountByName, "memes");
+
+            Assert.IsFalse(refCountByName.ContainsKey("memes"));
+        }
+
+        [TestMethod]
+        public void DecrementExistingReference()
+        {
+            ConcurrentDictionary<string, int> refCountByName = new ConcurrentDictionary<string, int>();
+            ReferenceCounting.IncrementReference(refCountByName, "memes");
+            ReferenceCounting.DecrementReference(refCountByName, "memes");
+
+            Assert.AreEqual(0, refCountByName["memes"]);
+        }
+    }
+}
---
+++ Test Projects/SFGraphicsTest/Tests/ReferenceCountTests/GetObjectsWithNoReferencesTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using SFGraphics.GLObjects.GLObjectManagement;
+
+namespace SFGraphicsTest.Tests.ReferenceCountTests
+{
+    [TestClass]
+    public class GetObjectsWithNoReferencesTests
+    {
+        [TestMethod]
+        public void OneObjectWithNoReferences()
+        {
+            ConcurrentDictionary<string, int> refCountByName = new ConcurrentDictionary<string, int>();
+            refCountByName.TryAdd("a", 1);
+            refCountByName.TryAdd("b", 0);
+            refCountByName.TryAdd("c", 1);
+
+            HashSet<string> namesWithNoReferences = ReferenceCounting.GetObjectsWithNoReferences(refCountByName);
+            Assert.AreEqual(1, namesWithNoReferences.Count);
+            Assert.IsTrue(namesWithNoReferences.Contains("b"));
+        }
+
+        [TestMethod]
+        public void DuplicateObjecstWithNoReferences()
+        {
+            ConcurrentDictionary<string, int> refCountByName = new ConcurrentDictionary<string, int>();
+            refCountByName.TryAdd("a", 0);
+            refCountByName.TryAdd("a", 0);
+            refCountByName.TryAdd("a", 0);
+
+            // Names are unique, so there is no need to delete an OpenGL object more than once.
+            HashSet<string> namesWithNoReferences = ReferenceCounting.GetObjectsWithNoReferences(refCountByName);
+            Assert.AreEqual(1, namesWithNoReferences.Count);
+            Assert.IsTrue(namesWithNoReferences.Contains("a"));
+        }
+
+        [TestMethod]
+        public void EmptyDictionary()
+        {
+            ConcurrentDictionary<string, int> refCountByName = new ConcurrentDictionary<string, int>();
+
+            HashSet<string> namesWithNoReferences = ReferenceCounting.GetObjectsWithNoReferences(refCountByName);
+            Assert.AreEqual(0, namesWithNoReferences.Count);
+        }
+    }
+}
