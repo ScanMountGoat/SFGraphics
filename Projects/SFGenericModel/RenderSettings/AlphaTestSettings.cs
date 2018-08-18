4567b2350b02c5221a5ba717c21f96ddc83e40cc
SMG <scandanavianmountaingoat@gmail.com>
2018-08-17 22:12:31 -0500

    sfgenericmodel project added

---
+++ Projects/SFGenericModel/RenderSettings/AlphaTestSettings.cs
@@ -0,0 +1,16 @@
+﻿using OpenTK.Graphics.OpenGL;
+
+namespace SFGenericModel.RenderState
+{
+    partial class RenderSettings
+    {
+        public class AlphaTestSettings
+        {
+            public bool enableAlphaTesting = false;
+
+            public AlphaFunction alphaFunction = AlphaFunction.Gequal;
+
+            public int referenceAlpha = 128;
+        }
+    }
+}
---
+++ Projects/SFGenericModel/RenderSettings/FaceCullingSettings.cs
@@ -0,0 +1,14 @@
+﻿using OpenTK.Graphics.OpenGL;
+
+namespace SFGenericModel.RenderState
+{
+    partial class RenderSettings
+    {
+        public class FaceCullingSettings
+        {
+            public bool enableFaceCulling = true;
+
+            public CullFaceMode cullFaceMode = CullFaceMode.Back;
+        }
+    }
+}
