f87c3a5bde40c528e1214ff79b08b32cdf2cfa2a
SMG <scandanavianmountaingoat@gmail.com>
2018-08-18 13:41:35 -0500

    set sampler wrap mode and min/mag filter

--- Projects/SFGraphics/GLObjects/Samplers/SamplerObject.cs
+++ Projects/SFGraphics/GLObjects/Samplers/SamplerObject.cs
@@ -4,7 +4,8 @@ using OpenTK.Graphics.OpenGL;
 namespace SFGraphics.GLObjects.Samplers
 {
     /// <summary>
-    /// 
+    /// Allows for setting texture state per texture unit rather than per texture.
+    /// The texture parameters of the bound sampler object are used instead of the texture's parameters.
     /// </summary>
     public sealed class SamplerObject : GLObject
     {
@@ -14,7 +15,82 @@ namespace SFGraphics.GLObjects.Samplers
         public override GLObjectType ObjectType { get { return GLObjectType.SamplerObject; } }
 
         /// <summary>
-        /// 
+        /// Updates the SamplerParameter when set.
+        /// </summary>
+        public TextureMinFilter MinFilter
+        {
+            get { return minFilter; }
+            set
+            {
+                minFilter = value;
+                SetSamplerParameter(SamplerParameterName.TextureMinFilter, (int)value);
+            }
+        }
+        private TextureMinFilter minFilter;
+
+        /// <summary>
+        /// Updates the SamplerParameter when set.
+        /// </summary>
+        public TextureMagFilter MagFilter
+        {
+            get { return magFilter; }
+            set
+            {
+                magFilter = value;
+                SetSamplerParameter(SamplerParameterName.TextureMagFilter, (int)value);
+            }
+        }
+        private TextureMagFilter magFilter;
+
+        /// <summary>
+        /// Updates the SamplerParameter when set.
+        /// </summary>
+        public TextureWrapMode TextureWrapS
+        {
+            get { return textureWrapS; }
+            set
+            {
+                textureWrapS = value;
+                SetSamplerParameter(SamplerParameterName.TextureWrapS, (int)value);
+            }
+        }
+        private TextureWrapMode textureWrapS;
+
+        /// <summary>
+        /// Updates the SamplerParameter when set.
+        /// </summary>
+        public TextureWrapMode TextureWrapT
+        {
+            get { return textureWrapT; }
+            set
+            {
+                textureWrapT = value;
+                SetSamplerParameter(SamplerParameterName.TextureWrapT, (int)value);
+            }
+        }
+        private TextureWrapMode textureWrapT;
+
+        /// <summary>
+        /// Updates the SamplerParameter when set.
+        /// </summary>
+        public TextureWrapMode TextureWrapR
+        {
+            get { return textureWrapR; }
+            set
+            {
+                textureWrapR = value;
+                SetSamplerParameter(SamplerParameterName.TextureWrapR, (int)value);
+            }
+        }
+        private TextureWrapMode textureWrapR;
+
+        private void SetSamplerParameter(SamplerParameterName parameter, int value)
+        {
+            GL.SamplerParameter(Id, parameter, value);
+        }
+
+        /// <summary>
+        /// Creates an unitialized sampler object. 
         /// </summary>
         public SamplerObject() : base(GL.GenSampler())
         {
@@ -22,7 +98,8 @@ namespace SFGraphics.GLObjects.Samplers
         }
 
         /// <summary>
-        /// 
+        /// Binds the Id to <paramref name="textureUnit"/>.
+        /// This sampler's state will override the state of the bound texture.
         /// </summary>
         /// <param name="textureUnit"></param>
         public void Bind(int textureUnit)
