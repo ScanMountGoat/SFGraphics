303b6592f1d4def64960c1b8ae3da1e6a444e5dc
SMG <scandanavianmountaingoat@gmail.com>
2018-09-04 10:01:07 -0500

    test vertex attribute size

---
+++ Test Projects/SFGenericModel.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SFGenericModel.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SFGenericModel.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7be87b94-ca49-4f44-905a-298ded4884f9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test Projects/SFGenericModel.Test/Tests/AttribPointerUtilsTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SFGenericModel;
+using OpenTK.Graphics.OpenGL;
+
+namespace SFGenericModel.Test
+{
+    [TestClass]
+    public class AttribPointerUtilsTests
+    {
+        [TestMethod]
+        public void Byte()
+        {
+            CheckAttribPointerSize(sizeof(byte), VertexAttribPointerType.Byte);
+        }
+
+        [TestMethod]
+        public void UnsignedByte()
+        {
+            CheckAttribPointerSize(sizeof(byte), VertexAttribPointerType.UnsignedByte);
+        }
+
+        [TestMethod]
+        public void Int()
+        {
+            CheckAttribPointerSize(sizeof(int), VertexAttribPointerType.Int);
+        }
+
+        [TestMethod]
+        public void UnsignedInt()
+        {
+            CheckAttribPointerSize(sizeof(int), VertexAttribPointerType.UnsignedInt);
+        }
+
+        [TestMethod]
+        public void Short()
+        {
+            CheckAttribPointerSize(sizeof(short), VertexAttribPointerType.Short);
+        }
+
+        [TestMethod]
+        public void UnsignedShort()
+        {
+            CheckAttribPointerSize(sizeof(short), VertexAttribPointerType.UnsignedShort);
+        }
+
+        [TestMethod]
+        public void Float()
+        {
+            CheckAttribPointerSize(sizeof(float), VertexAttribPointerType.Float);
+        }
+
+        [TestMethod]
+        public void Double()
+        {
+            CheckAttribPointerSize(sizeof(double), VertexAttribPointerType.Double);
+        }
+
+        private static void CheckAttribPointerSize(int expected, VertexAttribPointerType type)
+        {
+            Assert.AreEqual(expected, AttribPointerUtils.sizeInBytesByType[type]);
+        }
+    }
+}
