6b591b9794e201320bb04cd4c15832212331d057
SMG <scandanavianmountaingoat@gmail.com>
2018-09-15 20:23:42 -0500

    organize tests

---
+++ Test Projects/SFGraphics.Utils.Test/Test/VectorToolsTests/GetDegrees.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SFGraphics.Utils;
+
+namespace VectorToolsTests
+{
+    [TestClass]
+    public class GetDegrees
+    {
+        private readonly double delta = 0.00001;
+
+        [TestMethod]
+        public void ZeroRadiansToDegrees()
+        {
+            Assert.AreEqual(0, VectorUtils.GetDegrees(0), delta);
+        }
+
+        [TestMethod]
+        public void SmallRadiansToDegrees()
+        {
+            Assert.AreEqual(35, VectorUtils.GetDegrees(0.6108652382), delta);
+        }
+
+        [TestMethod]
+        public void LargeRadiansToDegrees()
+        {
+            Assert.AreEqual(720, VectorUtils.GetDegrees(12.566370614), delta);
+        }
+    }
+}
---
+++ Test Projects/SFGraphics.Utils.Test/Test/VectorToolsTests/GetRadians.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SFGraphics.Utils;
+
+namespace VectorToolsTests
+{
+    [TestClass]
+    public class GetRadians
+    {
+        private readonly double delta = 0.00001;
+
+        [TestMethod]
+        public void ZeroDegreesToRadians()
+        {
+            Assert.AreEqual(0, VectorUtils.GetRadians(0), delta);
+        }
+
+        [TestMethod]
+        public void SmallDegreesToRadians()
+        {
+            Assert.AreEqual(0.6108652382, VectorUtils.GetRadians(35), delta);
+        }
+
+        [TestMethod]
+        public void LargeDegreesToRadians()
+        {
+            Assert.AreEqual(12.566370614, VectorUtils.GetRadians(720), delta);
+        }
+    }
+}
