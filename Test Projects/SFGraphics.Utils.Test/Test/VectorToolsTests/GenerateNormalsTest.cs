c443f69dcb6d33e34f8049d1c0d6e657f14a79b2
SMG <scandanavianmountaingoat@gmail.com>
2018-09-15 20:12:43 -0500

    test generated values for normals

--- Test Projects/SFGraphics.Utils.Test/Test/VectorToolsTests/GenerateNormalsTest.cs
+++ Test Projects/SFGraphics.Utils.Test/Test/VectorToolsTests/GenerateNormalsTest.cs
@@ -16,8 +16,11 @@ namespace VectorToolsTests
                 Vector3 v1 = new Vector3(-5, 5, 1);
                 Vector3 v2 = new Vector3(-5, 0, 1);
                 Vector3 v3 = new Vector3(0, 0, 1);
-                Vector3 normal = VectorUtils.CalculateNormal(v1, v2, v3);
-                Assert.IsTrue(Vector3.Dot(normal, new Vector3(1)) > 0);
+                Vector3 normal = VectorUtils.CalculateNormal(v1, v2, v3).Normalized();
+
+                Assert.AreEqual(0, normal.X);
+                Assert.AreEqual(0, normal.Y);
+                Assert.AreEqual(1, normal.Z);
             }
 
             [TestMethod]
@@ -27,8 +30,11 @@ namespace VectorToolsTests
                 Vector3 v1 = new Vector3(-5, 5, 1);
                 Vector3 v2 = new Vector3(-5, 0, 1);
                 Vector3 v3 = new Vector3(0, 0, 1);
-                Vector3 normal = VectorUtils.CalculateNormal(v3, v2, v1);
-                Assert.IsTrue(Vector3.Dot(normal, new Vector3(1)) < 0);
+                Vector3 normal = VectorUtils.CalculateNormal(v3, v2, v1).Normalized();
+
+                Assert.AreEqual(0, normal.X);
+                Assert.AreEqual(0, normal.Y);
+                Assert.AreEqual(-1, normal.Z);
             }
         }
     }
