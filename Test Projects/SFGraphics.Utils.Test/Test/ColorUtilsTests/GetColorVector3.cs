aba085b0168f36f4285df89608a5c3a32fc7afd7
SMG <scandanavianmountaingoat@gmail.com>
2018-10-11 11:18:53 -0500

    test get color from vector3

---
+++ Test Projects/SFGraphics.Utils.Test/Test/ColorUtilsTests/GetColorVector3.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SFGraphics.Utils;
+using OpenTK;
+using System.Drawing;
+
+namespace ColorUtilsTests
+{
+    [TestClass]
+    public class GetColorVector3
+    {
+        [TestMethod]
+        public void WithinRange()
+        {
+            Assert.AreEqual(Color.FromArgb(255, 127, 0, 255), ColorUtils.ColorFromVector3(new Vector3(0.5f, 0, 1)));
+        }
+
+        [TestMethod]
+        public void BelowRange()
+        {
+            Assert.AreEqual(Color.FromArgb(255, 0, 0, 0), ColorUtils.ColorFromVector3(new Vector3(-1, -1, -1)));
+        }
+
+        [TestMethod]
+        public void AboveRange()
+        {
+            Assert.AreEqual(Color.FromArgb(255, 255, 255, 255), ColorUtils.ColorFromVector3(new Vector3(1.1f, 1.1f, 1.1f)));
+        }
+    }
+}
