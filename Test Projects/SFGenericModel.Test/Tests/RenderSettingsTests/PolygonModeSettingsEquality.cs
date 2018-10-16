cd428d94048b0a89a3e0c916d5c944d928e98a15
SMG <scandanavianmountaingoat@gmail.com>
2018-10-16 17:33:29 -0500

    add polygon mode equality operators

---
+++ Test Projects/SFGenericModel.Test/Tests/RenderSettingsTests/PolygonModeSettingsEquality.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SFGenericModel.RenderState;
+
+namespace RenderSettingsTests
+{
+    [TestClass]
+    public class PolygonModeSettingsEquality
+    {
+        [TestMethod]
+        public void SameObject()
+        {
+            var settings = new PolygonModeSettings();
+            var settings2 = settings;
+
+            Assert.IsTrue(settings.Equals(settings2));
+            Assert.IsTrue(settings == settings2);
+            Assert.IsFalse(settings != settings2);
+        }
+
+        [TestMethod]
+        public void DifferentObjects()
+        {
+            var settings = new PolygonModeSettings();
+            var settings2 = new PolygonModeSettings(OpenTK.Graphics.OpenGL.MaterialFace.Front, OpenTK.Graphics.OpenGL.PolygonMode.Point);
+
+            Assert.IsFalse(settings.Equals(settings2));
+            Assert.IsFalse(settings == settings2);
+            Assert.IsTrue(settings != settings2);
+        }
+    }
+}
