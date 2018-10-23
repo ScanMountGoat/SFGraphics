6e863890a277a3e2d4a4a65018cb53ed4f9722ee
SMG <scandanavianmountaingoat@gmail.com>
2018-10-23 16:58:33 -0500

    add bounding spheres 3d test case

--- Test Projects/SFGraphics.Utils.Test/Test/BoundingSphereTests/GenerateBoundingSphereFromSpheres.cs
+++ Test Projects/SFGraphics.Utils.Test/Test/BoundingSphereTests/GenerateBoundingSphereFromSpheres.cs
@@ -35,7 +35,7 @@ namespace BoundingSphereTests
         }
 
         [TestMethod]
-        public void DifferentRadiusDifferentCenter2D()
+        public void DifferentCenter2D()
         {
             var spheres = new List<Vector4>()
             {
@@ -43,6 +43,21 @@ namespace BoundingSphereTests
                 new Vector4(-1, 0, 0, 1),
                 new Vector4(0, 1, 0, 1),
                 new Vector4(0, -1, 0, 1),
+            };
+
+            var boundingSphere = BoundingSphereGenerator.GenerateBoundingSphere(spheres);
+            Assert.IsTrue(SpherePointUtils.SphereContainsSpheres(boundingSphere, spheres));
+        }
+
+        [TestMethod]
+        public void DifferentCenter3D()
+        {
+            var spheres = new List<Vector4>()
+            {
+                new Vector4(1, 0, 1, 1),
+                new Vector4(-1, 0, 1, 1),
+                new Vector4(-1, 2, -1, 1),
+                new Vector4(1, 2, -1, 1),
             };
 
             var boundingSphere = BoundingSphereGenerator.GenerateBoundingSphere(spheres);
