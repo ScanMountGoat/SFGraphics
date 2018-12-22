c4d22ce6c945a73cd6d8855c99d802cda6af1fbd
SMG <scandanavianmountaingoat@gmail.com>
2018-12-22 10:48:11 -0600

    create 3d texture class

---
+++ Projects/SFGraphics/GLObjects/Textures/Texture3D.cs
@@ -0,0 +1,43 @@
+﻿using OpenTK.Graphics.OpenGL;
+
+namespace SFGraphics.GLObjects.Textures
+{
+    /// <summary>
+    /// A TextureTarget.Texture3D texture designed for 3D lookup tables.
+    /// </summary>
+    public class Texture3D : Texture
+    {
+        /// <summary>
+        /// The depth of the base mip level in pixels.
+        /// </summary>
+        public int Depth { get; protected set; }
+
+        /// <summary>
+        /// Creates an empty 3D texture. 
+        /// The texture is incomplete until the dimensions and format are set.
+        /// </summary>
+        public Texture3D() : base(TextureTarget.Texture3D)
+        {
+
+        }
+
+        /// <summary>
+        /// Loads uncompressed texture data of the specified format for the first mip level.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="depth"></param>
+        /// <param name="imageData"></param>
+        /// <param name="format"></param>
+        public void LoadImageData<T>(int width, int height, int depth, T[] imageData, TextureFormats.TextureFormatUncompressed format) where T : struct
+        {
+            Width = width;
+            Height = height;
+            Depth = depth;
+
+            Bind();
+            GL.TexImage3D(TextureTarget, 0, format.pixelInternalFormat, width, height, depth, 0, format.pixelFormat, format.pixelType, imageData);
+        }
+    }
+}
---
+++ Test Projects/SFGraphics.Test/Tests/TextureTests/Texture3DTests/LoadImageData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenTK.Graphics.OpenGL;
+using SFGraphics.GLObjects.Textures;
+using SFGraphics.GLObjects.Textures.TextureFormats;
+
+namespace TextureTests.Texture3DTests
+{
+    [TestClass]
+    public class LoadImageData : Tests.ContextTest
+    {
+        // 2 x 4 x 8 RGBA byte.
+        private readonly byte[] imageData = new byte[2 * 4 * 8 * 4];
+
+        private Texture3D texture;
+
+        [TestInitialize()]
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            if (texture == null)
+                texture = new Texture3D();
+        }
+     
+        [TestMethod]
+        public void SetDimensions()
+        {
+            texture.LoadImageData(2, 4, 8, imageData, new TextureFormatUncompressed(PixelInternalFormat.Rgba, PixelFormat.Rgba, PixelType.Byte));
+
+            Assert.AreEqual(2, texture.Width);
+            Assert.AreEqual(4, texture.Height);
+            Assert.AreEqual(8, texture.Depth);
+        }
+    }
+}
