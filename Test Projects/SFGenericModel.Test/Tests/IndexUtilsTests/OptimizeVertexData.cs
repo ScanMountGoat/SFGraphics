4276581f193194f317ffdee567967e47b560808c
SMG <scandanavianmountaingoat@gmail.com>
2018-12-22 19:23:33 -0600

    add additional unit tests for generated indices

--- Test Projects/SFGenericModel.Test/Tests/IndexUtilsTests/OptimizeVertexData.cs
+++ Test Projects/SFGenericModel.Test/Tests/IndexUtilsTests/OptimizeVertexData.cs
@@ -15,5 +15,35 @@ namespace SFGenericModel.Utils.Test.IndexUtilsTests
             CollectionAssert.AreEqual(vertices, newVertices);
             CollectionAssert.AreEqual(new List<int>() { 0 }, newIndices);
         }
+
+        [TestMethod]
+        public void ThreeIdenticalVertices()
+        {
+            var vertices = new List<char>() { 'a', 'a', 'a' };
+            IndexUtils.OptimizedVertexData(vertices, out List<char> newVertices, out List<int> newIndices);
+
+            CollectionAssert.AreEqual(new List<char>() { 'a' }, newVertices);
+            CollectionAssert.AreEqual(new List<int>() { 0, 0, 0 }, newIndices);
+        }
+
+        [TestMethod]
+        public void ThreeUniqueVertices()
+        {
+            var vertices = new List<char>() { 'a', 'b', 'c' };
+            IndexUtils.OptimizedVertexData(vertices, out List<char> newVertices, out List<int> newIndices);
+
+            CollectionAssert.AreEqual(new List<char>() { 'a', 'b', 'c' }, newVertices);
+            CollectionAssert.AreEqual(new List<int>() { 0, 1, 2 }, newIndices);
+        }
+
+        [TestMethod]
+        public void RepeatedAndUniqueVertices()
+        {
+            var vertices = new List<char>() { 'a', 'b', 'd', 'b', 'b', 'c', 'c' };
+            IndexUtils.OptimizedVertexData(vertices, out List<char> newVertices, out List<int> newIndices);
+
+            CollectionAssert.AreEqual(new List<char>() { 'a', 'b', 'd', 'c' }, newVertices);
+            CollectionAssert.AreEqual(new List<int>() { 0, 1, 2, 1, 1, 3, 3 }, newIndices);
+        }
     }
 }
