84ff89aa19b1d2955e03e968c4ab8fc654495fa4
SMG <scandanavianmountaingoat@gmail.com>
2020-07-09 16:37:46 -0500

    add sampler parameters, closes #177

--- Projects/SFGraphics/GLObjects/Samplers/SamplerObject.cs
+++ Projects/SFGraphics/GLObjects/Samplers/SamplerObject.cs
@@ -11,77 +11,42 @@ namespace SFGraphics.GLObjects.Samplers
         internal override GLObjectType ObjectType => GLObjectType.SamplerObject;
 
         /// <summary>
-        /// Updates the SamplerParameter when set.
+        /// Updates the SamplerParameter.
         /// </summary>
-        public TextureMinFilter MinFilter
-        {
-            get => minFilter;
-            set
-            {
-                minFilter = value;
-                SetSamplerParameter(SamplerParameterName.TextureMinFilter, (int)value);
-            }
-        }
-        private TextureMinFilter minFilter;
+        public TextureMinFilter MinFilter { set => SetSamplerParameter(SamplerParameterName.TextureMagFilter, (int)value); }
 
         /// <summary>
-        /// Updates the SamplerParameter when set.
+        /// Updates the SamplerParameter.
         /// </summary>
-        public TextureMagFilter MagFilter
-        {
-            get => magFilter;
-            set
-            {
-                magFilter = value;
-                SetSamplerParameter(SamplerParameterName.TextureMagFilter, (int)value);
-            }
-        }
-        private TextureMagFilter magFilter;
+        public TextureMagFilter MagFilter { set => SetSamplerParameter(SamplerParameterName.TextureMagFilter, (int)value); }
 
         /// <summary>
-        /// Updates the SamplerParameter when set.
+        /// Updates the SamplerParameter.
         /// </summary>
-        public TextureWrapMode TextureWrapS
-        {
-            get => textureWrapS;
-            set
-            {
-                textureWrapS = value;
-                SetSamplerParameter(SamplerParameterName.TextureWrapS, (int)value);
-            }
-        }
-        private TextureWrapMode textureWrapS;
+        public TextureWrapMode TextureWrapS { set => SetSamplerParameter(SamplerParameterName.TextureWrapS, (int)value); }
 
         /// <summary>
-        /// Updates the SamplerParameter when set.
+        /// Updates the SamplerParameter.
         /// </summary>
-        public TextureWrapMode TextureWrapT
-        {
-            get => textureWrapT;
-            set
-            {
-                textureWrapT = value;
-                SetSamplerParameter(SamplerParameterName.TextureWrapT, (int)value);
-            }
-        }
-        private TextureWrapMode textureWrapT;
+        public TextureWrapMode TextureWrapT { set => SetSamplerParameter(SamplerParameterName.TextureWrapT, (int)value); }
 
         /// <summary>
-        /// Updates the SamplerParameter when set.
+        /// Updates the SamplerParameter.
         /// </summary>
-        public TextureWrapMode TextureWrapR
-        {
-            get => textureWrapR;
-            set
-            {
-                textureWrapR = value;
-                SetSamplerParameter(SamplerParameterName.TextureWrapR, (int)value);
-            }
-        }
-        private TextureWrapMode textureWrapR;
+        public TextureWrapMode TextureWrapR { set => SetSamplerParameter(SamplerParameterName.TextureWrapR, (int)value); }
 
         /// <summary>
-        /// Creates an unitialized sampler object. 
+        /// Updates the SamplerParameter.
+        /// </summary>
+        public float TextureMaxAnisotropy { set => SetSamplerParameter(SamplerParameterName.TextureMaxAnisotropyExt, value); }
+
+        /// <summary>
+        /// Updates the SamplerParameter.
+        /// </summary>
+        public float TextureLodBias { set => SetSamplerParameter(SamplerParameterName.TextureLodBias, value); }
+
+        /// <summary>
+        /// Creates an unitialized sampler.
         /// </summary>
         public SamplerObject() : base(GL.GenSampler())
         {
@@ -92,13 +57,18 @@ namespace SFGraphics.GLObjects.Samplers
         /// Binds the Id to <paramref name="textureUnit"/>.
         /// This sampler's state will override the state of the bound texture.
         /// </summary>
-        /// <param name="textureUnit"></param>
+        /// <param name="textureUnit">The target texture unit for binding</param>
         public void Bind(int textureUnit)
         {
             GL.BindSampler(textureUnit, Id);
         }
 
         private void SetSamplerParameter(SamplerParameterName parameter, int value)
+        {
+            GL.SamplerParameter(Id, parameter, value);
+        }
+
+        private void SetSamplerParameter(SamplerParameterName parameter, float value)
         {
             GL.SamplerParameter(Id, parameter, value);
         }
---
+++ Test Projects/SFGraphics.Test/Tests/SamplerTests/SetProperties.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenTK.Graphics.OpenGL;
+using SFGraphics.GLObjects.Samplers;
+
+namespace SFGraphics.Test.SamplerTests
+{
+    [TestClass]
+    public class SetProperties : GraphicsContextTest
+    {
+        [TestMethod]
+        public void TextureWrapS()
+        {
+            var sampler = new SamplerObject { TextureWrapS = TextureWrapMode.MirroredRepeat };
+            GL.GetSamplerParameter(sampler.Id, SamplerParameterName.TextureWrapS, out int param);
+            Assert.AreEqual((int)TextureWrapMode.MirroredRepeat, param);
+        }
+
+        [TestMethod]
+        public void TextureWrapT()
+        {
+            var sampler = new SamplerObject { TextureWrapT = TextureWrapMode.MirroredRepeat };
+            GL.GetSamplerParameter(sampler.Id, SamplerParameterName.TextureWrapT, out int param);
+            Assert.AreEqual((int)TextureWrapMode.MirroredRepeat, param);
+        }
+
+        [TestMethod]
+        public void TextureWrapR()
+        {
+            var sampler = new SamplerObject { TextureWrapR = TextureWrapMode.MirroredRepeat };
+            GL.GetSamplerParameter(sampler.Id, SamplerParameterName.TextureWrapR, out int param);
+            Assert.AreEqual((int)TextureWrapMode.MirroredRepeat, param);
+        }
+
+        [TestMethod]
+        public void TextureLodBias()
+        {
+            var sampler = new SamplerObject { TextureLodBias = -1.234f };
+            GL.GetSamplerParameter(sampler.Id, SamplerParameterName.TextureLodBias, out float param);
+            Assert.AreEqual(-1.234f, param);
+        }
+
+        [TestMethod]
+        public void TextureMaxAnisotropy()
+        {
+            var sampler = new SamplerObject { TextureMaxAnisotropy = 16f };
+            GL.GetSamplerParameter(sampler.Id, SamplerParameterName.TextureMaxAnisotropyExt, out float param);
+            Assert.AreEqual(16f, param);
+        }
+    }
+}
